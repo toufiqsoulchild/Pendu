6e3f5afacd7e3bf55b2507c833755bbf903d8f5f
toufiqsoulchild <57954630+toufiqsoulchild@users.noreply.github.com>
2021-08-01 22:23:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 import os
 from fonction import dictionnaire, mystere
 
+
 motmystere = dictionnaire()
-## mot = input("Veuillez saisir un mot pou une lettre : ")
 motcache = mystere(motmystere)
+motmystere = list(motmystere)
+motcache = list(motcache)
+taille = len(motmystere)
+occurence = 0
+essai = 7
+fin = True
+
+print(motmystere)
+print(motcache)
+
+while fin == True:
+    saisie = input("\nVeuillez saisie une lettre : ")
+    occurence = 0
+    
+    for i in range(taille):
+        j = i
+        if saisie == motmystere[i] :
+            motcache[j] = saisie
+            occurence = occurence + 1
+        else: 
+            pass
+
+    if occurence == 0:
+        print ("raté\n")
+        essai = essai - 1 
+    else:
+        pass
+
+    if essai > 0
+        pass
+    elif essai == 0:
+        print("tu as perdu !")
+        fin == False
+        
+    print(f"\nil te reste {essai} essais.\n")
+    print(str(motcache))
